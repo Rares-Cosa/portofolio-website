06c0b9aab2909e7df074469ed7de0a92e4ed1e07
Cosa Andrei-Rares <64634481+Rares-Cosa@users.noreply.github.com>
2023-04-04 14:29:16 +0300

    Update React project card
    Importing the proper background and update the href for Code button to the
    portofolio repo on GitHub.

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import WorkImg from '../assets/workImg.jpeg'
-import realEstate from '../assets/realestate.jpg'
+import PortofolioImg from '../assets/portofolioImg.png'
 
 const Work = () => {
   return (
@@ -18,20 +18,21 @@ const Work = () => {
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
 
                 {/* Card Item */}
-                <div style={{backgroundImage: `url(${WorkImg})`}} className='shadow-lg shadow-[#040c16] group container flex justify-center items-center mx-auto content-div'>
+                <div style={{backgroundImage: `url(${PortofolioImg})`}} className='shadow-lg shadow-[#040c16] group container flex justify-center items-center mx-auto content-div border-4 border-[#D5BDC3]'>
 
                     {/* Hover Effects */}
                     <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold tracking-wider text-white'>
+                        <span className='text-2xl font-bold tracking-wider text-white text-center'>
                             React JS Application
                         </span>
+                        <p className='font-bold text-white text-center'>(You are looking to it right now)</p>
 
                         <div className='pt-8 text-center'>
 
                             <a href='/'>
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-[#6FA788] font-bold text-lg'>Demo</button>
                             </a>
-                            <a href='/'>
+                            <a href='https://github.com/Rares-Cosa/portofolio-website' target='_blank' rel="noreferrer">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-[#6FA788] font-bold text-lg'>Code</button>
                             </a>
 
